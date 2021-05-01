275fee2acedf980f25777f3505fac9ce475d2442
Juan Prieto <82191535+shakerfinal@users.noreply.github.com>
2021-05-01 17:04:13 -0500

    Add files via upload

---
+++ precache-manifest.f580cff713344fcff9f961217ca5806f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edcd5b62e04b26c40d2771312babde79",
+    "url": "/camilaproyecto/index.html"
+  },
+  {
+    "revision": "a7bf71e1f70b56f0e8e5",
+    "url": "/camilaproyecto/static/css/main.7dc85121.chunk.css"
+  },
+  {
+    "revision": "107db8289a22134b638f",
+    "url": "/camilaproyecto/static/js/2.dacd06c0.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/camilaproyecto/static/js/2.dacd06c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7bf71e1f70b56f0e8e5",
+    "url": "/camilaproyecto/static/js/main.df5a9445.chunk.js"
+  },
+  {
+    "revision": "15b6a8a17e3e05215d79",
+    "url": "/camilaproyecto/static/js/runtime-main.845b9545.js"
+  },
+  {
+    "revision": "917c560c9bcbeb4df3596704ef26b20c",
+    "url": "/camilaproyecto/static/media/micheile-henderson-_HIlJ_popSQ-unsplash.917c560c.jpg"
+  }
+]);
